ad5417f43aa90cedc5c0f2a5350a82f378a02153
Bryant Omoregie <unclebryant@gmail.com>
2019-12-31 03:00:05 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,5 @@
-row = ["1", "2", "3"]# Define the variable board below.
----------------------
-row = ["4", "5", "6"]
----------------------
-row = ["7","8", "9"]
----------------------
+board = ["1", "2", "3"]# Define the variable board below.
+
+board = ["4", "5", "6"]
+
+board = ["7", "8", "9"]
